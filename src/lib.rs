d7e36a7c07d4fe4270e5ae60c2936506a7c282ad
Lucas Robidou <lucas.robidou@uni-bielefeld.de>
2025-11-11 16:57:11 +0100

    add compute_all_superkmers_linear_streaming to public functions

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,5 @@ mod two_bits;
 
 type Minimizer = u64;
 
+pub use crate::superkmers_computation::compute_all_superkmers_linear_streaming;
 pub use crate::superkmers_computation::compute_superkmers_linear_streaming;
-
-// pub use lines_iter::LinesIter;
